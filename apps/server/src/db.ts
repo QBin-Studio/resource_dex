5104d0aa615dc0475e2e7deb006dc41ae74df145
Mrbns <hi@mrbns.dev>
2025-09-14 13:58:00 +0600

    fixed database client issue and pnpm to bun

--- apps/server/src/db.ts
+++ apps/server/src/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../prisma/client/index.js";
+import { PrismaClient } from '../prisma/client/client.js';
 const dbClient = new PrismaClient();
 
 export default dbClient;
